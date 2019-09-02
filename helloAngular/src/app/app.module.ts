e1e75ee40126f3e82609f1fdc90c9b939bf9afa0
imranulmean <imranulhasan73@gmail.com>
2019-09-02 17:25:30 +0600

    updated

--- helloAngular/src/app/app.module.ts
+++ helloAngular/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ helloAngular/src/app/components/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  sendReq(){
+        this.httpClient.get("http://localhost:3000/api/getData/1").subscribe((data)=>{
+            console.log(data);
+        });  	
+  }
+}
